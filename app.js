8e207b742419f68dbdc717673a66ddeeeca9a0a1
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-07-26 14:54:13 +0530

    Initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
